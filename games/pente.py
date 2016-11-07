7454701ab070b3111d71efa7e9008eb495eebca6
Javeria Khan <javeria.ak@gmail.com>
2016-11-07 01:46:44 -0800

    Pente Update
    Signed-off-by: Javeria Khan <javeria.ak@gmail.com>

--- games/pente.py
+++ games/pente.py
@@ -3,9 +3,9 @@
 
 import os
 import sys
+import re
 
 # Draw 15x15 Pente Board
-
 def DrawBoard(rowNumber = 3,colNumber = 3):
     #global Board
     os.system('clear')
@@ -25,12 +25,16 @@ def DrawBoard(rowNumber = 3,colNumber = 3):
         for piece in range(0,colNumber):
             print "|",
             print('\x1b[1;32;40m'+"%s"%Board[row-1][piece]+'\x1b[0m'),
-            #%s"%Board[row-1][piece],
         print '|'
 
         #Print Horizontal Borders
         print '  ',
         print("+---"*colNumber+"+")
+
+    #Print Moves History
+    print "\nMoves by X:"
+    for move in X_moves:
+        print "(%s) "%move,
 
     return
 
@@ -40,6 +44,9 @@ if __name__ == "__main__":
     size = "15x15"
     sizeList = []
     global Board
+
+    X_moves = []
+
     if "x" or "*" in size:
         sizeList = (str(size).replace("x","*")).split("*")
     else:
@@ -54,31 +61,30 @@ if __name__ == "__main__":
         colNumber = int(sizeList[0])
         rowNumber = colNumber
     
-
     # Initialize Board
     Board = [[' ' for i in range(1,rowNumber+1)] for j in range(1,colNumber+1)]
-
     
     DrawBoard(rowNumber,colNumber)
 
-
     #Take input
     while True:
-        print "\nEnter q to quit"
+        print "\n\nEnter q to quit"
         coordinates = raw_input("\nPlayer turn. Enter location to place piece (x,y): ")
         if coordinates.lower() == 'q':
           print('\x1b[1;31;40m' + '\n\nEnding Game....\n\n' + '\x1b[0m')
           sys.exit(0)
+        elif re.match("^(a{1,15},a{1,15})$", coordinates, re.M|re.I)
         else:
+            #log all moves for player-X
+            X_moves.append(coordinates)
 
+            #Update Board with X's latest move
             coor_list = (coordinates.split(","))
             x_cor = int(coor_list[0])
             y_cor = int(coor_list[1])
-
-            print "x: %s"%x_cor
-            print "y: %d"%y_cor
             Board[x_cor-1][y_cor-1] = 'X'
 
+            #Redraw Board
             DrawBoard(rowNumber,colNumber)
 
 
